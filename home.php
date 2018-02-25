dcde0dc9c58829990fe27f2986c53e401f8caa1e
Ryo Nakagome <nakagome@tento-net.com>
2018-02-25 12:17:46 +0900

    ホーム画面完成

--- home.php
+++ home.php
@@ -49,27 +49,61 @@
       <li><a href="r-qrcheck.php">QRコード</a></li>
       <li><a href="r-kobetsu.php">個別注文</a></li>
     </ul>
-    <ul id="d-"
+    <ul id="d-orgfood" class="dropdown-content">
+      <li><a href="of-list.php">団体・食品一覧</a></li>
+      <li><a href="o-add.php">団体追加</a></li>
+      <li><a href="f-add.php">食品追加</a></li>
+      <li><a href="s-check.php">ステータスチェック</a></li>
+    </ul>
+    <ul id="slide-out" class="sidenav">
+      <li>
+        <div class="user-view">
+          <div class="background">
+            <img width="100%" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Yamabuki_High_School.JPG/1200px-Yamabuki_High_School.JPG">
+          </div>
+          <a href="#!user"><img class="circle" src="http://www.yamabuki-hs.metro.tokyo.jp/site/tei/content/000026901.jpg"></a>
+          <a href="#!name" style="color: white;">
+            <?php
+              $UserID = $_SESSION['UserID'];
+              require_once('config/config.php');
+              $sql = mysqli_query($db_link, "SELECT UserName FROM tp_user_booth WHERE UserID = '$UserID'");
+              $result = mysqli_fetch_assoc($sql);
+              print($result['UserName']);
+            ?>
+          </a>
+        </div>
+      </li>
+      <li><a href="#!" class="dropdown-trigger" data-target="d-recept">受付<i class="material-icons right">arrow_drop_down</i></a></li>
+      <li><a href="#!" class="dropdown-trigger" data-target="d-orgfood">データ管理<i class="material-icons right">arrow_drop_down</i></a></li>
+      <li><a href="#!" class="dropdown-trigger" data-target="d-userc">ユーザ管理<i class="material-icons right">arrow_drop_down</i></a></li>
+      <li class="divider"></li>
+      <li><a href="logout.php">ログアウト</a></li>
+    </ul>
     <nav class="light-blue darken-4">
       <div class="container">
         <div class="nav-wrapper">
           <a href="#!" class="brand-logo">Ticper</a>
-          <a href="#" data-target="mobilemenu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="r-modeselect.php">団体用Ticoer</a></li>
-            <li><a href="https://yamabuki.ticper.com">顧客用Ticper</a></li>
-          </ul>
+          <div class="right hide-on-med-and-down">
+            <a href="#!" onClick="var elem = document.querySelector('.sidenav');var instance = M.Sidenav.getInstance(elem);instance.open();" class="btn">メニューを開く</a>
+          </div>
+          <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         </div>
       </div>
     </nav>
-
-    <ul class="sidenav" id="mobilemenu">
-      <li><a href="https://org.yamabuki.ticper.com">団体用Ticper</a></li>
-      <li><a href="https://yamabuki.ticper.com">顧客用Ticper</a></li>
-    </ul>
-
     <script>
+      $(".dropdown-trigger").dropdown();
       $(document).ready(function(){
         $('.sidenav').sidenav();
+        M.toast({html: '<?php print($result['UserName']); ?>さんとしてログインしました。'})
       });
     </script>
+    <div class="container">
+      <div class="row">
+        <div class="row s12">
+          <h3>ようこそ</h3>
+          <p><?php print($result['UserName']); ?>さん、ようこそ。<br>メニューを開いて、アクションを選んでください。</p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
