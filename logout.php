4cc5c4ba566aee5129ae4f37017a37f744421248
Ryo Nakagome <nakagome@tento-net.com>
2018-03-21 21:00:02 +0900

    ログアウトのプログラムを追加

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
 	session_start();
 	session_destroy();
-	print('<script>alert("ログアウトしました。); location.href = "index.php";</script>');
+	print('<script>alert("ログアウトしました。"); location.href = "index.php";</script>');
 ?>
