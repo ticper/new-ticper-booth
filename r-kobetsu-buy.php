c5daaf6287c6d8efa1abf46bef7e125403e432b3
Ryo Nakagome <nakagome@tento-net.com>
2018-04-18 17:05:39 +0900

    個別注文が追加されない

---
+++ r-kobetsu-buy.php
@@ -0,0 +1,28 @@
+<?php
+    $CartID = $_POST['cartid'];
+    $Azukari = $_POST['get'];
+
+    require_once('config/config.php');
+
+    $sql = mysqli_query($db_link, "SELECT FoodID, Sheets FROM tp_kobetsu_carts WHERE CartID = '$CartID'");
+    while($result = mysqli_fetch_assoc($sql)) {
+        $sql2 = mysqli_query($db_link, "SELECT FoodPrice, FoodStock FROM tp_food WHERE FoodID = '$FoodID'");
+        $result2 = mysqli_fetch_assoc($sql2);
+        $sql2 = mysqli_query($db_link, "SELECT MAX(TicketID) AS num FROM tp_ticket");
+        $result2 = mysqli_fetch_assoc($sql2);
+        $ticketid = $result2['num'];
+        
+        // アクティベーションコードを生成
+        $ticketAcode = rand(100000, 999999);
+        
+        // 食品IDを取得
+        $FoodID = $result['FoodID'];
+        // 枚数を取得
+        $Sheets = $result['Sheets'];
+            
+        $sql2 = mysqli_query($db_link, "INSERT INTO tp_ticket VALUES ('$ticketid', '$ticketAcode', '', '$CartID', '$FoodID', '$Sheets', '0')");
+        $sql2 = mysqli_query($db_link, "UPDATE tp_food SET FoodStock = FoodStock - '$Sheets', Bought = Bought + '$Sheets' WHERE FoodID = '$FoodID'");
+    }
+    $_SESSION['CartID'] = "";
+    print('<script>location.href = "r-kobetsu-viewticket.php?cartid='.$CartID.'";</script>');
+?>
