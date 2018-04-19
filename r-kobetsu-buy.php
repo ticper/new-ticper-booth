39942017dd97da868e2dcecb8ec1813da9bdd742
yahagi-day <32870950+yahagi-day@users.noreply.github.com>
2018-04-19 22:46:52 +0900

    no message

--- r-kobetsu-buy.php
+++ r-kobetsu-buy.php
@@ -6,17 +6,18 @@
 
     $sql = mysqli_query($db_link, "SELECT FoodID, Sheets FROM tp_kobetsu_carts WHERE CartID = '$CartID'");
     while($result = mysqli_fetch_assoc($sql)) {
+        
+    	$FoodID = $result['FoodID'];
+
         $sql2 = mysqli_query($db_link, "SELECT FoodPrice, FoodStock FROM tp_food WHERE FoodID = '$FoodID'");
         $result2 = mysqli_fetch_assoc($sql2);
-        $sql2 = mysqli_query($db_link, "SELECT MAX(TicketID) AS num FROM tp_ticket");
+        $sql2 = mysqli_query($db_link, "SELECT MAX(TicketID)+1 AS num FROM tp_ticket");
         $result2 = mysqli_fetch_assoc($sql2);
         $ticketid = $result2['num'];
         
         // アクティベーションコードを生成
         $ticketAcode = rand(100000, 999999);
         
-        // 食品IDを取得
-        $FoodID = $result['FoodID'];
         // 枚数を取得
         $Sheets = $result['Sheets'];
             
