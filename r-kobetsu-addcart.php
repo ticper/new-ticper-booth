e434762bd0c690031f08a02f29b3a2148c97b3b7
yahagi_day <yahagi.day@tensi.org>
2018-05-26 01:16:49 +0900

    謎のミスを修正

--- r-kobetsu-addcart.php
+++ r-kobetsu-addcart.php
@@ -18,10 +18,10 @@
 
     $sql = mysqli_query($db_link, "SELECT Sheets FROM tp_kobetsu_carts WHERE FoodID = '$h_FoodID' AND CartID = '$CartID'");
     $result = mysqli_fetch_assoc($sql);
-    if($result['Sheets'] >= 1) {
+    if($result['Sheets'] != 0) {
       $sql = mysqli_query($db_link, "UPDATE tp_kobetsu_carts SET Sheets = Sheets + '1' WHERE CartID = '$CartID' AND FoodID = '$h_FoodID'");
     } else {
-      $sql = mysqli_query($db_link, "INSERT INTO tp_kobetsu_carts VALUES ('$CartID', '$h_FoodID', '1', '')");
+      $sql = mysqli_query($db_link, "INSERT INTO tp_kobetsu_carts VALUES ('$CartID', '$h_FoodID', '1')");
     }
     print('<script>location.href = "r-kobetsu.php";</script>');
 ?>
