0675d03f716d2e8b4a79fd6ae35fd8a0d63369bf
Guilty Agent <x86.taka@gmail.com>
2018-12-04 14:06:24 +0900

    ログインしていない状態でニュースの投稿ができる。 XSS, SQL Injectionの対策。

--- t-addnews.php
+++ t-addnews.php
@@ -1,6 +1,19 @@
 <?php
+    session_start();
+    if(isset($_SESSION['UserID']) == '') {
+        print('<script>location.href = "index.php";</script>');
+        exit();
+    }
+    
+    if (isset($_POST['news']) == ''){
+        print('<script>location.href = "index.php";</script>');
+        exit();
+    }
     $News = $_POST['news'];
+    $News = htmlspecialchars($News, ENT_QUOTES);
+
     require_once('config/config.php');
+    $News = $db_link -> real_escape_string($News);
     $sql = mysqli_query($db_link, "SELECT MAX(NewsID) AS num FROM tp_news");
     $result = mysqli_fetch_assoc($sql);
     $newsid = $result['num'] + 1;
--- t-news.php
+++ t-news.php
@@ -2,6 +2,7 @@
     session_start();
     if(isset($_SESSION['UserID']) == '') {
         print('<script>location.href = "index.php";</script>');
+        exit();
     } else {
         $UserID = $_SESSION['UserID'];
     }
